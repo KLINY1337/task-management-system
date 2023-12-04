60ef2d27ca935219006b5ec77e67eff89fbd383f
KLINY1337 <semyon.chernomurov@gmail.com>
2023-12-04 20:31:42 +0300

    Initial commit 1) Initialized base project 2) Configured base dependencies

---
+++ src/test/java/com/chernomurov/effectivemobile/test/task/management/system/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chernomurov.effectivemobile.test.task.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
